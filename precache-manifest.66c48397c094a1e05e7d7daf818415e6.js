a701561a97722d43a3e111ad2e5a413e95ea518e
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2020-08-12 12:36:12 +0100

    Updates

---
+++ precache-manifest.66c48397c094a1e05e7d7daf818415e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2840caec56fdf5f8b0262f61af3a98",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "60177ce2bef156fdfe45",
+    "url": "/monsters-rolodex/static/css/main.2bd75195.chunk.css"
+  },
+  {
+    "revision": "abd523e040d673da432f",
+    "url": "/monsters-rolodex/static/js/2.e63e2d79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-rolodex/static/js/2.e63e2d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60177ce2bef156fdfe45",
+    "url": "/monsters-rolodex/static/js/main.b647de08.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
